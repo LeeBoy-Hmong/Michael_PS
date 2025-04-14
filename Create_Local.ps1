ff8b41f4fc4d0b9100aec47c27676415cd88c9fb
LeeBoy-Hmong <michael1998lee@outlook.com>
2025-04-13 22:21:49 -0500

    Initial commit of Create_Local ps1 file

---
+++ Create_Local.ps1
@@ -0,0 +1,38 @@
+## Create Local Admin for all of the TBGKnaack setups
+
+# Configure profiles needed
+$UserName = "TBGKnaack"
+$PlainPassword = "Eltorito01!"
+$Group = "Administrators"
+$Description = "Admin Account for Tech Support"
+
+$SecurePassword = ConvertTo-SecureString $PlainPassword -AsPlainText -Force  # Convert the password to a SecureString
+
+# Checking if the user exist, if not - create the Local user
+if (-not (Get-LocalUser -Name $UserName -ErrorAction SilentlyContinue)) {
+	try {
+        New-LocalUser -Name $UserName `
+                    -Password $SecurePassword `
+                    -FullName $UserName `
+                    -Description $Description `
+                    -PasswordNeverExpires `
+                    -AccountNeverExpires `
+                    -UserMayNotChangePassword
+        
+        Write-Output "User $UserName was created successfully."
+    } catch {
+        Write-Output "Error creating user ${UserName}: $_"
+        exit 1
+    }
+} else {
+    Write-Output "User $UserName already exists in the system. Skipping creation."
+}
+
+# Error Handling
+try {
+    Add-LocalGroupMember -Group $Group -Member $UserName -ErrorAction Stop
+    Write-Output "User $UserName is now added to the $Group group."
+} catch {
+    Write-Output "Error adding user to group: $_"
+}
+

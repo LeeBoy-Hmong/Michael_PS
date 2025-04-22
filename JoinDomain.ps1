3b2b1b7d646b3079c3be2e49610d1af18fa8d052
LeeBoy-Hmong <michael1998lee@outlook.com>
2025-04-21 21:47:04 -0500

    started Create_local ps1

---
+++ JoinDomain.ps1
@@ -0,0 +1,9 @@
+# Define the Domain that is being joined
+$Domain = "BergDrwall"
+$Username = "BergDrywall\\TBGAdmin"
+$Password = "Eltorito01!"
+# Generate the hostname format for the joining device
+
+# Create secure credential object for user
+
+# Join the Domain with hostname created

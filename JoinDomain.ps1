24a29e3c1fdc13cdd97138cd3f64de7fbdf53de7
unknown <Michael1998Lee@Outlook.com>
2025-04-21 22:51:35 -0500

    domainjoined PS complete3

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -1,9 +1,23 @@
+
+
+
 # Define the Domain that is being joined
-$Domain = "BergDrwall"
+$Domain = "BergDrywall.local"
 $Username = "BergDrywall\\TBGAdmin"
 $Password = "Eltorito01!"
-# Generate the hostname format for the joining device
 
 # Create secure credential object for user
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force  # Securing your $Password variable
+$Credential = New-Object System.Management.Automation.PSCredential ($Username, $SecurePassword)
 
-# Join the Domain with hostname created
+# Path of joined computer to Active Directory
+$OUPath = "OU=Computers,OU=The Berg Group,DC=BergDrywall,DC=local"  # Put your path here where the device will be assigned to
+
+# Join the Domain with current name of device hostname created
+try {
+    Add-Computer -DomainName $Domain -Credential $Credential -OUPath $OUPath -Restart -Force
+    Write-Output "You've succesfully joined $env:COMPUTERNAME to $Domain. Your computer will now reboot"
+}
+catch {
+    Write-Output "Failed to join the domain: $($_.Exception.Message)"
+}
